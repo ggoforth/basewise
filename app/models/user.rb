d3a7b921f5f00f4728d810d39923ddf4306a4fb0
dphaener <dphaener@gmail.com>
2015-02-27 09:19:27 -0800

    Fixed has_secure_password tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,21 @@ class User < ActiveRecord::Base
   NAME_REGEX = /\A[a-zA-Z\-\!\@]+\z/
   EMAIL_REGEX = /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
 
-  attr_accessor :password
-
   validates :first_name, format: { with: NAME_REGEX }, length: (2..100)
   validates :last_name, format: { with: NAME_REGEX }, length: (1..100)
   validates :email, format: { with: EMAIL_REGEX }, uniqueness: true
-  validate :password_validity
+  validate :validate_password
+
+  has_secure_password validations: false
+
+  def self.authenticate(params = {})
+    return false unless user = User.find_by_email(params[:email])
+    user.authenticate(params[:password])
+  end
 
 private
 
-  def password_validity
+  def password_valid?
     unless password =~ /[0-9]+/
       errors.add(:password, "must contain at least one number")
     end
@@ -27,5 +32,9 @@ private
     unless (6..100).cover?(password.length)
       errors.add(:password, "must be at least 6 characters long")
     end
+  end
+
+  def validate_password
+    password_valid? if new_record? || password
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225022748) do
+ActiveRecord::Schema.define(version: 20150227045422) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20150225022748) do
     t.string   "email"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "password_digest"
+    t.text     "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
